2cf8b63fa5364e2ad524297fd9d0e1d1e733c2d3
Ryan H <rhurtis@hotmail.com>
2020-08-05 18:11:15 +0000

    Added a route for /urls in order to pass URL data to our template.

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -24,4 +25,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+// new route for /urls; this will pass URL data to our template
+app.get("/urls", (req,res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
