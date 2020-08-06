a7249b4d416ff271779c6a4e2de52e67ad8ac073
Ryan H <rhurtis@hotmail.com>
2020-08-06 22:29:18 +0000

    Added the ability to login

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
@@ -33,21 +37,30 @@ app.get("/hello", (req, res) => {
 
 // new route for /urls; this will pass URL data to our template
 app.get("/urls", (req,res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase,
+  username: req.cookies['username']
+ };
   res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
 });
 
 // post request for generating a new url
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  console.log('got here')
+  console.log('req.body',req.body);  // Log the POST request body to the console
   urlDatabase[generateRandomString()] = req.body['longURL'];
-  console.log(urlDatabase);
-  res.send(`/urls/${Object.keys(urlDatabase)[Object.values(urlDatabase).indexOf(req.body['longURL'])]}`);         // Respond with 'Ok' (we will replace this)
+  console.log('this is the url database',urlDatabase);
+  //res.send(`/urls/${Object.keys(urlDatabase)[Object.values(urlDatabase).indexOf(req.body['longURL'])]}`);         // Respond with 'Ok' (we will replace this)
+  res.redirect('/urls');
 });
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = { 
+    username: req.cookies['username']
+   };
+  res.render("urls_new",templateVars);
+});
+
+
 
 // post request for deleting urls
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -68,6 +81,16 @@ app.post("/urls/:shortURL", (req, res) => {
   //res.send(`/urls/${Object.keys(urlDatabase)[Object.values(urlDatabase).indexOf(req.body['longURL'])]}`);
   res.redirect('/urls');
 })
+
+// post request for logging in
+app.post("/login", (req, res) => {
+  console.log('a login attempt was made');
+  console.log(req.body);  // Log the POST request body to the console
+  
+  res.cookie('username',req.body.username);
+  res.redirect('/urls');
+  
+});
 
 
 
@@ -99,7 +122,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],  username: req.cookies['username']  };
   res.render("urls_show", templateVars);
 });
 
