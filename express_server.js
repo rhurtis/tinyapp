869835d3436f8510f9c02e7d5d374027089eddd3
Ryan H <rhurtis@hotmail.com>
2020-08-06 03:29:14 +0000

    added a function that will generate a random string of 6 characters.

--- express_server.js
+++ express_server.js
@@ -53,5 +53,5 @@ app.get("/urls/:shortURL", (req, res) => {
 
 
 function generateRandomString() {
-
+  return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2,5);
 }
